deae9f0d7eed60ecde3dff31b6feff6f472eb7a0
David Martin <d.r.martin.wrk@gmail.com>
2023-05-15 16:03:32 -0400

    routes

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const app = express();
+const port = 3000
 
-app.listen(3000, () => {
+//Routes 
+app.get('/cats', (req, res) => {
+res.send('This is the get route for all cats!');
+})
+
+// POST a new cat
+app.post('/cats', (req, res) => {
+    res.send('This is the POST route for a new cat!');
+})
+
+// GET a single cat
+app.get('/cats/:id', (req, res) => {
+    res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
+});
+
+// PUT
+app.put('/cats/:id', (req, res) => {
+    res.send(`This is the GET route for a single cat found at ${ req.params.id }`);
+});
+
+// DESTROY
+app.delete('/cats/:id', (req, res) => {
+    res.send(`This is the GET route for a single cat found at ${ req.params.id }, bye bye`);
+});
+
+app.listen(port, () => {
     console.log('I am listening at port 3000')
 });
+
